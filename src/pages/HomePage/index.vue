f6639b5c6627b1d3396b4235478b93f631602b71
Vincent Liao <vincent.liao@protonmail.com>
2019-09-19 11:22:05 +0700

    very simple and bad looking v1

--- src/pages/HomePage/index.vue
+++ src/pages/HomePage/index.vue
@@ -1,23 +1,48 @@
 <template>
   <div>
-    <post-list></post-list>
-    <button class="button" @click="get_something">test get</button>
+    <button class="button" @click="get_data">show</button>
+    <single-post v-for="post in posts"
+      :key="post.id" 
+      :author="post.user" 
+      :points="post.points" 
+      :title="post.title" 
+    ></single-post>
   </div>
 </template>
 
 <script>
 import PostList from './PostList'
+import SinglePost from './SinglePost'
 import api from '@/api'
+import { mapMutations } from 'vuex'
 
 export default {
   components: {
-    PostList
+    PostList,
+    SinglePost
   },
-  methods: {
-    async get_something(){
-      const response = await api.get('news', '1.json')
-      console.log(response)
+
+  data(){
+    return {
+      posts: null
     }
+  },
+  
+  methods: {
+    ...mapMutations([
+      'SET_POST'
+    ]),
+    get_data(){
+      console.log(this.posts)
+    }
+  },
+
+  async mounted(){
+    const response = await api.get('news', '1.json')
+    this.posts = response.data 
+
+    // this is kinda useless I guess...
+    // this.SET_POST(response.data)
   }
 }
 </script>
