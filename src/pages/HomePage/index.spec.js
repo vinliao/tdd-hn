91e0629c01f90f5ee0bb1870d2ec1d9edfe428f8
Vincent Liao <vincent.liao@protonmail.com>
2019-09-24 11:14:04 +0700

    add dispatch and getters test for homepage/index.vue

--- src/pages/HomePage/index.spec.js
+++ src/pages/HomePage/index.spec.js
@@ -3,61 +3,46 @@ import HomePage from '@/pages/HomePage'
 import api from '@/api'
 
 describe('HomePage', () => {
-  // FIXME: having these two $store is breaking the whole thing,
-  // this is one consequence of not using localvue
-  // using a fake store and mock it
+
+  // setup the test by mocking the store
+  api.get = jest.fn(() => Promise.resolve({}))
+  const posts = [{}, {}, {}]
+  const $store = {
+    dispatch: jest.fn(),
+    getters: {
+      get_post: jest.fn(() => posts)
+    }
+  }
+
+  const wrapper = shallowMount(HomePage, {
+    mocks: {
+      $store
+    }
+  })
+
   it('dispatch to get main page', async () => {
-    api.get = jest.fn(() => Promise.resolve({}))
-    const $store = {
-      dispatch: jest.fn(),
-    } 
-
-    shallowMount(HomePage, {
-      mocks: {
-        $store
-      }
-    })
-
     const first_dispatch = {
       type: 'news',
       page: 1
     }
 
     expect($store.dispatch).toHaveBeenCalledWith('get_post', first_dispatch)
+    // this asserts that the getters are working fine
+    expect(wrapper.vm.posts()).toEqual(posts)
+
+    // I originally wanted to assert this
+    // expect($store.getters.get_post).toHaveBeenCalled()
+    // but for some reason it just doesn't work, I have on idea why
+
   })
 
-  it('renders the right amount of items from getters', () => {
-    const posts = [{}, {}, {}]
-    const $store = {
-      getters: {
-        // get_post: jest.fn(() => posts)
-        get_post: jest.fn()
-      }
-    }
+  it('passes the right props to child component', () => {
+    const post_list = wrapper.find({ name: 'post-list' })
+
+    // the post needs a parantheses because it's still
+    // in the form of "function"
+    expect(post_list.props().posts()).toEqual(posts)
   })
-
-  // it('renders the right amount of item (from getters)', () => {
-  //   const posts = [{}, {}, {}]
-  //   const $store = {
-  //     getters: {
-  //       get_post: jest.fn(() => posts)
-  //     },
-  //     dispatch: jest.fn(),
-  //   }  
-
-  //   const wrapper = shallowMount(HomePage, {
-  //     mocks: {
-  //       $store
-  //     }
-  //   })
-
-  //   // const post_list = wrapper.find({ name: 'post-list' })
-  //   // expect(post_list.exists()).toBe(true)
-  //   // const single_items = wrapper.findAll({ name: 'single-post' }) 
-  //   // expect(single_items).toHaveLength(posts.length)
-
-  //   // expect post list passed with props...
-  // })
 
   // Another alternative is using the real store
   // https://lmiller1990.github.io/vue-testing-handbook/vuex-in-components-mutations-and-actions.html#testing-with-a-real-vuex-store
